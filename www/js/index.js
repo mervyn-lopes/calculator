75fa285e7b92a61b9f25787794d4fa79be285cb8
mervyn <mervynlopes@gmail.com>
2013-05-28 13:25:07 +0530

    Create index.js

---
+++ www/js/index.js
@@ -0,0 +1,19 @@
+function calculate(a,b,op)
+{
+  if(op=="plus")
+  {
+    return a+b;
+  }
+  if(op=="minus")
+  {
+    return a-b;
+  }
+  if(op=="multiply")
+  {
+    return a*b;
+  }
+  if(op=="divide")
+  {
+    return a/b;
+  }
+}
